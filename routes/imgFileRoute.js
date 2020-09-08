14739ca8b52d9e75900c0b7cc10142f850f7cfdb
Natalia Alexandra <natalia_alexandra@web.de>
2020-09-08 22:32:57 +0200

    route

--- routes/imgFileRoute.js
+++ routes/imgFileRoute.js
@@ -1,12 +1,11 @@
 const route = require("express").Router()
-const {
-    getImgFiles, getImgFile, postImgFile, putImgFile, deleteImgFile
-} = require("../controllers/imgFileController")
+const {getImgFiles, getImgFile, postImgFile, putImgFile, deleteImgFile} = require("../controllers/imgFileController")
+const imgFileUpload = require("../middleware/imgFileUpload")
 
 route.get("/", getImgFiles)
 route.get("/:id", getImgFile)
-route.get("/", postImgFile)
-route.get("/:id", putImgFile)
-route.get("/:id", deleteImgFile)
+route.post("/upload", imgFileUpload, postImgFile)
+route.put("/:id/:filename", imgFileUpload, putImgFile)
+route.delete("/:id/:filename", deleteImgFile)
 
 module.exports = route
